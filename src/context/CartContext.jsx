c90f05485f502db6bfc70d2f68b4905a2b6e76f6
ltherisod <lauratherisod@gmail.com>
2025-11-25 21:19:46 -0300

    carrito persistente

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,10 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext()
 
+const carritoLS = JSON.parse(localStorage.getItem('carrito')) || []
 export const CartProvider = ({children})=> {
-    const [cart, setCart]= useState([])
+    const [cart, setCart]= useState(carritoLS)
 
+    //seteamos y actualizamos el local storage cada vez que el carrito cambia
+        useEffect(()=>{
+            localStorage.setItem('carrito', JSON.stringify(cart))
+        },[cart])
+        
     console.log(cart, 'carrito')
     //agregar un item al carrito, contempla duplicados
     const addItem = (item,qty)=>{
